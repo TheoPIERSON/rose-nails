b5977794f3c84618d02635018d83cea4d38d4d91
Theo Pierson <thheopierson@gmail.com>
2024-06-21 19:23:05 +0200

    feat: move hero to components folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { HeroComponent } from './hero/hero.component';
+import { HeroComponent } from './components/hero/hero.component';
 import { BackgroundComponent } from './static/background/background.component';
 import { HomepageComponent } from './pages/homepage/homepage.component';
 
@@ -14,13 +14,10 @@ import { HomepageComponent } from './pages/homepage/homepage.component';
     HeaderComponent,
     HeroComponent,
     BackgroundComponent,
-    HomepageComponent
+    HomepageComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
